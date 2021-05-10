0f4b7c8a7ad513bf2622d348eeff3a1508e21fbc
José Miguel Ribeiro Cunha <miguel_cunha_99@hotmail.com>
2021-05-10 16:37:30 +0100

    Added base project

---
+++ src/test/java/com/zuehlke/dependencyinjection/DependencyInjectionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zuehlke.dependencyinjection;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DependencyInjectionApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
