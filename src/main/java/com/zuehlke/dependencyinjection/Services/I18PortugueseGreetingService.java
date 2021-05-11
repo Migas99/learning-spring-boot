21783b850b2f556c2ae6a7bfce16a24f4d3170ce
José Miguel Ribeiro Cunha <miguel_cunha_99@hotmail.com>
2021-05-11 08:45:47 +0100

    Defined default profile

--- src/main/java/com/zuehlke/dependencyinjection/Services/I18PortugueseGreetingService.java
+++ src/main/java/com/zuehlke/dependencyinjection/Services/I18PortugueseGreetingService.java
@@ -3,7 +3,7 @@ package com.zuehlke.dependencyinjection.Services;
 import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Service;
 
-@Profile("PT")
+@Profile({"PT", "default"})
 @Service("i18nService")
 public class I18PortugueseGreetingService implements IGreetingService {
 
