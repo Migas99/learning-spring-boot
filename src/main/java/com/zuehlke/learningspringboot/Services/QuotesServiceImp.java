385bb1fdb813af17b53230bb9b6e0b3668cee9f5
José Miguel Ribeiro Cunha <miguel_cunha_99@hotmail.com>
2021-05-11 16:43:56 +0100

    Implemented QuotesService

--- src/main/java/com/zuehlke/learningspringboot/Services/QuotesServiceImp.java
+++ src/main/java/com/zuehlke/learningspringboot/Services/QuotesServiceImp.java
@@ -1,13 +1,20 @@
 package com.zuehlke.learningspringboot.Services;
 
+import guru.springframework.norris.chuck.ChuckNorrisQuotes;
 import org.springframework.stereotype.Service;
 
 @Service
 public class QuotesServiceImp implements QuotesService {
 
+    private final ChuckNorrisQuotes quotes;
+
+    public QuotesServiceImp() {
+        this.quotes = new ChuckNorrisQuotes();
+    }
+
     @Override
     public String getRandomQuote() {
-        return null;
+        return this.quotes.getRandomQuote();
     }
 
 }
